9c678231279e6070be3d2bbf0465e8c4a7db6d98
Amperchik <aleksejjrdnv@rambler.ru>
2021-11-09 17:38:32 +0300

    Исправлены замечания(через бины и dependency injection)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,21 +1,26 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
-public class CarContorller {
+public class CarController {
+    final
+    CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam("count") int count) {
-        model.addAttribute("message", new CarService().getListCars(count));
+        model.addAttribute("message", carService.getListCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,23 +1,28 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarService {
+    List<Car> cars;
+
+    public CarService() {
+        this.cars = new ArrayList<>();
+    }
+
     public List<String> getListCars(int count){
-        List<Car> cars=new ArrayList<>();
-        cars.add(new Car("BMW", 1, "Vasia"));
-        cars.add(new Car("Audi", 2, "Petya"));
-        cars.add(new Car("Skoda", 3, "Kolya"));
-        cars.add(new Car("YAZ", 4, "Masha"));
-        cars.add(new Car("VOLGA", 5, "Senya"));
-        List<String> request = cars.stream().
+        cars.add(new Car("BMW", 1, "owner1"));
+        cars.add(new Car("Audi", 2, "owner2"));
+        cars.add(new Car("Mercedes", 3, "owner3"));
+        cars.add(new Car("YAZ", 4, "owner4"));
+        cars.add(new Car("VOLGA", 5, "owner5"));
+        return cars.stream().
                 limit(count).
-                map(x->x.toString()).
+                map(Car::toString).
                 collect(Collectors.toList());
-        return request;
     }
 }
